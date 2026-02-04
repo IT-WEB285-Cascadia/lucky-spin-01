c56369491b10f8f3f49e618ac8b3be02e5492f37
Leif Biery <leifbiery@student.cascadia.edu>
2026-02-03 17:08:20 -0800

    completes part 4

--- LuckySpin/Program.cs
+++ LuckySpin/Program.cs
@@ -4,7 +4,7 @@ using Microsoft.Extensions.DependencyInjection.Extensions;
 var builder = WebApplication.CreateBuilder(args);
 
 // Install Services using the builder.Services methods
-  //  TODO: include the builder service "AddControllers" to enable MVC controllers
+  //  DONE: include the builder service "AddControllers" to enable MVC controllers
 builder.Services.AddControllers();
 
 //Builds the app with the added services
@@ -19,7 +19,7 @@ app.UseStaticFiles();
 if (!app.Environment.IsDevelopment()) {
     app.UseExceptionHandler("/Spinner/Error");
 }
-//   TODO: include the app method "UseRouting" to recognize custom "Routes" in place of folders and files
+//   DONE: include the app method "UseRouting" to recognize custom "Routes" in place of folders and files
 app.UseRouting();
 
 
@@ -34,8 +34,6 @@ app.MapControllerRoute(
         controller = "Spinner",
         action = "Index",
         luck = 7
-        // I've made changes
     });
 
 app.Run();
-
